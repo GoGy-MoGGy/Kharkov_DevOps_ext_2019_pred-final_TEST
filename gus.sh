9832586096359d715b46c4c8c1e521c301c162ac
GoGy-MoGGy <31355899+GoGy-MoGGy@users.noreply.github.com>
2020-03-17 22:54:52 +0200

    Add files via upload

---
+++ gus.sh
@@ -0,0 +1,5 @@
+ #!/bin/bash/
+sudo rm -rf /var/www/html/* ;
+cd ~ ;
+git clone https://github.com/GoGy-MoGGy/Kharkov_DevOps_ext_2019_pred-final_TEST/ ; 5. sudo cp ~/Kharkov_DevOps_ext_2019_pred-final_TEST/* /var/www/html/ ;
+sudo rm -rf ~/Kharkov_DevOps_ext_2019_pred-final_TEST ;
